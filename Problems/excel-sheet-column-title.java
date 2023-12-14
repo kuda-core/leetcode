ce9a42272b98ddd91e9d4155edbd8d7f4a6fc006
kuda-core <147682634+kuda-core@users.noreply.github.com>
2023-12-13 18:04:05 -0800

    Create excel-sheet-column-title.java

---
+++ Problems/excel-sheet-column-title.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        //A + 1 (-1) = A
+        char c = '_';
+        String ret = "";
+        while(columnNumber>0) {
+            int remainder = (columnNumber - 1) % 26;
+            System.out.println("A + " + remainder);
+            ret += (char)('A' + remainder);
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        StringBuilder input1 = new StringBuilder();
+ 
+        // append a string into StringBuilder input1
+        input1.append(ret);
+ 
+        // reverse StringBuilder input1
+        input1.reverse();
+
+        return input1.toString();
+    }
+}
